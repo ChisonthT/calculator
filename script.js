0becaa2cc13d95d5598df352a6f72acbed46293b
Chisonth <1533494+ChisonthT@users.noreply.github.com>
2024-04-09 17:26:25 -0400

    Added basic calculation functionality.

---
+++ script.js
@@ -0,0 +1,20 @@
+function add(a,b){
+    console.log(a+b);
+}
+
+function sub(a,b){
+    console.log(a-b);
+}
+
+function mul(a,b){
+    console.log(a*b);
+}
+
+function div(a,b){
+    console.log(a/b);
+}
+
+add(5,4);
+sub(5,4);
+mul(5,4);
+div(5,4);
