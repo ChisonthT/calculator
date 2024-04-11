b9bcba79658c9b36e11c88f404ab1a3bab7ef182
Chisonth <1533494+ChisonthT@users.noreply.github.com>
2024-04-11 13:47:32 -0400

    Calculator functions with the input of number, operator, number, and equal sign.

--- script.js
+++ script.js
@@ -1,20 +1,85 @@
-function add(a,b){
-    console.log(a+b);
+function add(firstNum, secNum){
+    return(firstNum+secNum);
 }
 
-function sub(a,b){
-    console.log(a-b);
+function sub(firstNum, secNum){
+    return(firstNum-secNum);
 }
 
-function mul(a,b){
-    console.log(a*b);
+function mul(firstNum, secNum){
+    return(firstNum*secNum);
 }
 
-function div(a,b){
-    console.log(a/b);
+function div(firstNum, secNum){
+    return(firstNum/secNum);
 }
 
-add(5,4);
-sub(5,4);
-mul(5,4);
-div(5,4);
+function operation(operator, firstNum, secNum){
+    let result = '';
+
+    switch(operator){
+        case 'add':
+            result = add(firstNum, secNum);
+            break;
+        case 'subtract':
+            result = sub(firstNum, secNum);
+            break;
+        case 'multiply':
+            result = mul(firstNum, secNum);
+            break;
+        case 'divide':
+            if (secNum == 0){
+                result = "Error Can't Divide by 0!!";
+            }else{
+                result = div(firstNum, secNum);
+            }
+            break;
+    }
+
+    console.log(result);
+}
+
+
+let tempNum = '';
+let firstNum = '';
+let secNum = '';
+let operator = ''; 
+let display = document.querySelector('p');
+
+
+let btns = document.querySelector('#btns');
+
+
+btns.addEventListener("click",(e)=> {
+
+    if (e.target.classList.contains('clear')){
+        display.textContent = '0';
+        firstNum = '';
+        secNum = '';
+        operator = '';
+    }
+    
+    
+    if (e.target.classList.contains('equal')){
+        secNum = Number(tempNum);
+
+        operation(operator, firstNum, secNum);
+    }
+
+    if (e.target.classList.contains('number')) {
+        display.textContent += e.target.textContent;
+        
+        tempNum = tempNum + e.target.textContent;
+    }
+
+    if ((e.target.classList[1] == 'add') ||
+        (e.target.classList[1] == 'subtract') ||
+        (e.target.classList[1] == 'multiply') ||
+        (e.target.classList[1] == 'divide')){
+            display.textContent += e.target.textContent;
+            operator = e.target.classList[1];
+            firstNum = Number(tempNum);
+            console.log(firstNum);
+            tempNum = '';
+        }
+})
